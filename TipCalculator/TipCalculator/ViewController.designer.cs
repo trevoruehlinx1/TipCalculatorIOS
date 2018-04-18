5c1c102db1d078b63370e2b4f7c37f507316b6a9
Your Name <you@example.com>
2018-04-18 13:40:42 -0700

    Worked on GUI

--- TipCalculator/TipCalculator/ViewController.designer.cs
+++ TipCalculator/TipCalculator/ViewController.designer.cs
@@ -1,4 +1,4 @@
-﻿// WARNING
+// WARNING
 //
 // This file has been generated automatically by Visual Studio from the outlets and
 // actions declared in your storyboard file.
@@ -13,8 +13,79 @@ namespace TipCalculator
     [Register ("ViewController")]
     partial class ViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField checkAmountInputTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider serviceSlider { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel taxAmountOutputLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField taxPercentInputTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISwitch taxToggle { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel tipAmountOutputLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel tipPercetageOutputLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel totalCheckAmountOutputLabel { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (checkAmountInputTextField != null) {
+                checkAmountInputTextField.Dispose ();
+                checkAmountInputTextField = null;
+            }
+
+            if (serviceSlider != null) {
+                serviceSlider.Dispose ();
+                serviceSlider = null;
+            }
+
+            if (taxAmountOutputLabel != null) {
+                taxAmountOutputLabel.Dispose ();
+                taxAmountOutputLabel = null;
+            }
+
+            if (taxPercentInputTextField != null) {
+                taxPercentInputTextField.Dispose ();
+                taxPercentInputTextField = null;
+            }
+
+            if (taxToggle != null) {
+                taxToggle.Dispose ();
+                taxToggle = null;
+            }
+
+            if (tipAmountOutputLabel != null) {
+                tipAmountOutputLabel.Dispose ();
+                tipAmountOutputLabel = null;
+            }
+
+            if (tipPercetageOutputLabel != null) {
+                tipPercetageOutputLabel.Dispose ();
+                tipPercetageOutputLabel = null;
+            }
+
+            if (totalCheckAmountOutputLabel != null) {
+                totalCheckAmountOutputLabel.Dispose ();
+                totalCheckAmountOutputLabel = null;
+            }
         }
     }
 }
